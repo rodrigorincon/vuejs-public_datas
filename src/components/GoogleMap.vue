d346d1b04b28be5b10e8210ab34adce344224ccd
rodrigo rincon <rodrigo.rincon@altran-ais.com.br>
2018-12-10 01:35:47 -0200

    tirando mais codigo que permaneceu do conflito

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -20,8 +20,7 @@
 export default {
   props: {
     ubs_to_mark: { type: Array },
-    zoom: { default: 3, type: Number },
-    calcRoute: { default: false, type: Boolean }
+    zoom: { default: 3, type: Number }
   },
   data() {
     return {
@@ -30,69 +29,19 @@ export default {
   },
   mounted(){
     this.$refs.map.options.maxZoom = 18
-    if(this.calcRoute){
-      this.getMyPosition()
-      this.calcRouteToUbs()
-    }
   },
   watch:{
-    ubs_to_mark: function(new_markers, old_markers){
-      if(new_markers.length == 1 && new_markers[0].cod_cnes !== null && this.$vnode.key == 2){
-        const bounds = new google.maps.LatLngBounds()
-        new_markers.forEach(ubs => {
-          bounds.extend(ubs.position())
-        })
-        this.$refs.map.fitBounds(bounds)
-        if(this.calcRoute){
-          this.getMyPosition()
-          this.calcRouteToUbs()
-        }
-      }
+    ubs_to_mark: function(new_markers){
+      const bounds = new google.maps.LatLngBounds()
+      new_markers.forEach(ubs => {
+        bounds.extend(ubs.position())
+      })
+      this.$refs.map.fitBounds(bounds)
     }
   },
   methods: {
     showUbsDetails: function(id){
       this.$router.push('/ubs/'+id)
-    },
-    getMyPosition: function() {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-      });
-      const bounds = new google.maps.LatLngBounds()
-      bounds.extend(this.center)
-      bounds.extend(this.ubs_to_mark[0].position())
-      this.$refs.map.fitBounds(bounds)
-      console.log("aqui")
-      console.log(this.ubs_to_mark[0].position())
-      console.log(this.center)
-    },
-    calcRouteToUbs: function(){
-      var map = new google.maps.Map(document.getElementById('map'), {})
-      console.log(this.$refs.map)
-      console.log(map)
-      var directionsDisplay = new google.maps.DirectionsRenderer({
-          map: this.$refs.map.$mapObject
-      });
-      var directionsService = new google.maps.DirectionsService
-      directionsService.route({
-        origin: this.center,
-        destination: this.ubs_to_mark[0].position(),
-        travelMode: google.maps.TravelMode.DRIVING,
-        provideRouteAlternatives: true,
-        avoidTolls: true,
-        avoidFerries: true,
-        avoidHighways: false
-      },function (response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-            //directionsDisplay.setDirections(response);
-            console.log("funfou")
-        } else {
-            alert('Não foi possível calcular uma rota até a UBS ' + status);
-        }
-      });
     }
   }
 };
