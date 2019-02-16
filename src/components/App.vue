609e3fb19eb1813de67bb9ed3f79c5d5a0415608
Carlos <am53_coliveira@uolinc.com>
2019-02-16 20:19:27 -0200

    Removido código desnecessário

--- src/components/App.vue
+++ src/components/App.vue
@@ -5,7 +5,7 @@
       <div v-if="!loading && ubs_list.length > 0">
         <pagination :list="ubs_list" :perPage="25" firstBtnText="Primeiros" lastBtnText="Últimos" prevBtnText="Anteiores" nextBtnText="Próximos">
           <template slot-scope="props">
-            <google-map v-if="!hasKey" :ubs_to_mark="props.listPaginated" />
+            <google-map :ubs_to_mark="props.listPaginated" />
             <ubs-table :ubs_list="props.listPaginated"></ubs-table>
           </template>
         </pagination>
@@ -25,14 +25,8 @@ import Search from "./Search.vue"
 import UbsTable from "./UbsTable.vue"
 import GoogleMap from "./GoogleMap.vue";
 import Pagination from "./Pagination.vue";
-import { GOOGLE_API_KEY } from '../util/secrets'
 
 export default {
-  data: function () {
-    return {
-      hasKey: !Boolean(GOOGLE_API_KEY)
-    }
-  },
   props: ["ubs_list", "loading"],
   components:{
     Search,
